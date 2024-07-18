b02f1be557014867304def5e4748fb0d69cd4024
pablosingh <pablo22_4@hotmail.com>
2024-07-18 19:36:05 -0300

    agregado de configureStore y combineReducers

--- regoperaciones/src/redux/reducers.js
+++ regoperaciones/src/redux/reducers.js
@@ -2,9 +2,9 @@ import { combineReducers } from 'redux';
 import opReducer from './opReducer';
 import userReducer from './userReducer';
 
-const reducers = combineReducers({
+const rootReducers = combineReducers({
     opReducer: opReducer,
     userReducer: userReducer,
 });
 
-export default reducers;
+export default rootReducers;
--- regoperaciones/src/redux/store.js
+++ regoperaciones/src/redux/store.js
@@ -1,9 +1,8 @@
-// import { configureStore } from "reduxjs/toolkit";
-import { createStore } from "redux";
-import reducers from "./reducers";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducers from "./reducers";
 
-const store = createStore(
-    reducers,
-);
+const store = configureStore({
+    reducer: rootReducers,
+});
 
 export default store;
